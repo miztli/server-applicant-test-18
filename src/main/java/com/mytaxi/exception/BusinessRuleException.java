59eadeeb651ffb9074d9b53906da891d466faf3f
miztli <dark_coneja@hotmail.com>
2018-10-14 14:55:20 -0500

    Add new Exceptions

---
+++ src/main/java/com/mytaxi/exception/BusinessRuleException.java
@@ -0,0 +1,18 @@
+package com.mytaxi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class BusinessRuleException extends Exception {
+
+    private static final long serialVersionUID = 5764155525791162969L;
+
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+
+    public BusinessRuleException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
