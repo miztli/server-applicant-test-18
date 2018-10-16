cd828cc326801a3b1fee586af177a9c9a967e2cf
miztli <dark_coneja@hotmail.com>
2018-10-16 01:27:41 -0500

    Change HTTP error code

--- src/main/java/com/mytaxi/exception/CarAlreadyInUseException.java
+++ src/main/java/com/mytaxi/exception/CarAlreadyInUseException.java
@@ -3,7 +3,7 @@ package com.mytaxi.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class CarAlreadyInUseException extends Exception {
 
 
